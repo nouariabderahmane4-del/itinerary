285d1f10234e6e456b1cafb2f4c189d7c198f313
Abderahmane Nouari <nouariabderahmane4@gmail.com>
2025-10-04 23:10:11 +0300

    first steps into the tool

--- main.go
+++ main.go
@@ -8,6 +8,39 @@ import (
 
 const usage = "itinerary usage:\ngo run . ./input.txt ./output.txt ./airport-lookup.csv"
 
+type Airport struct {
+	Name string
+	City string
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func main(){
 	if len(os.Args) == 2 && os.Args[1] == "-h"{
 	fmt.Println(usage)
@@ -16,7 +49,30 @@ func main(){
 
 	if len(os.Args) != 4 {
 		fmt.Println(usage)
+		os.Exit(1) //Failure
+	}
+
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+	lookupPath := os.Args[3]
+
+	_, err := os.ReadFile(inputPath)
+	if err != nil {
+		if os.IsNotExist(err){
+			fmt.Println("Input not found")
+			os.Exit(1)
+		}
+		fmt.Println("Error reading input file: %v\n", err)
 		os.Exit(1)
 	}
-	
+
+	_, err := os.ReadFile(lookupPath)
+	if err != nil {
+		if os.IsNotExist(err){
+			fmt.Println("Airport lookup  not found")
+			os.Exit(1)
+		}
+		fmt.Println("Error reading Airport lookup file: %v\n", err)
+		os.Exit(1)
+	}
 }
