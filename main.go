f55cd7aab57978f3fcf15b9834f2c8524ad249a7
Abderahmane Nouari <nouariabderahmane4@gmail.com>
2025-10-08 21:00:19 +0300

    building

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 		fmt.Println("itinerary usage:\ngo run . ./input.txt ./output.txt ./airport-lookup.csv")
 	}else if len(os.Args) != 4 {
 		fmt.Println("Wrong number of arguments. Use -h for help")
+		os.Exit(0)
+	}else if os.Args[1] != "./input.txt" || os.Args[2] != "./output.txt" || os.Args[3] != "./airport_lookup.csv"{
+		fmt.Println("one or more arguments are wrong. Use -h for help")
+		os.Exit(0)
 	}
+	fmt.Println("Proceeding with the program")
+
 
 }
