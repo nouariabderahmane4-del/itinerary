b0bbbd98531ce89b4dfd15f65357c0d597e68eef
Abderahmane Nouari <nouariabderahmane4@gmail.com>
2025-10-08 20:22:00 +0300

    -h handled

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	asking_for_help := os.Args[1]
+	if asking_for_help == "-h" {
+		fmt.Println("itinerary usage:\ngo run . ./input.txt ./output.txt ./airport-lookup.csv")
+	}
+
+}
